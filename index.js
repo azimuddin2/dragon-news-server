e7a8b87e0c4d982e5232e6eb5f1595d9ddc8a0e7
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-02-05 20:59:37 +0600

    added is news and categories data

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === 'all-news') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.get('/', (req, res) => {
